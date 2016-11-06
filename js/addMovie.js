7675e1257687c2422f5a723c81372889a0264fc7
andy <andy@localhost.localdomain>
2016-11-06 14:42:57 -0600

    addmovie form complete

--- js/addMovie.js
+++ js/addMovie.js
@@ -1,6 +1,15 @@
 
 $(document).ready(function(){
 
+  //populate year select
+  var opt = '';
+  for (i = 2016; i > 1940; i= i-1) {
+    opt += '<option value=\"'+i+'\">'+i+'</option>';
+  }
 
+  $("#movieYear").append(opt);
+
+  //submit
+  
 
 });
