52ac80749bc2b1bfe1c85864be033cd9c5a53cdf
andy <andy@localhost.localdomain>
2016-11-06 17:57:36 -0600

    probing git ignore

--- data/connection.php
+++ data/connection.php
@@ -1,8 +1,8 @@
 <?php
 function connectionToDataBase(){
 		$servername = "localhost";
-		$username = "root";
-		$password = "root";
+		$username = "andy";
+		$password = "";
 		$dbname = "moviewave";
 
 
