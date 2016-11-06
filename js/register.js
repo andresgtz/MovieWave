44eefb3c0ef8bd430ebae0bf76deab02094580bf
Jesusnm4 <jesusanavarrom@gmail.com>
2016-11-06 14:45:06 -0600

    added Layers

--- js/register.js
+++ js/register.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
+	$("#register").on("click", function() {
+            var jsonObject = {
+                "username": $("#register-username").val(),
+                "userPassword": $("#register-password1").val(),
+                "confirmPassword": $("#register-password2").val(),
+                "userEmail": $("#register-email-input").val(),
+                "action" : "register"
+            };
 
-
-
+            $.ajax({
+                type: "POST",
+                url: "data/applicationLayer.php",
+                data: jsonObject,
+                dataType: "json",
+                contentType: "application/x-www-form-urlencoded",
+                success: function(jsonData) {
+                    alert(jsonData);
+                },
+                error: function(errorMsg) {
+                    alert(errorMsg.statusText);
+                }
+            });
 });
