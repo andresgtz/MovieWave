17e4a926c6a67895cdaa9256b8ede2d42073a32e
andy <andy@localhost.localdomain>
2016-11-06 15:29:26 -0600

    succes and error msgs working

--- js/addMovie.js
+++ js/addMovie.js
@@ -1,5 +1,8 @@
 
 $(document).ready(function(){
+  //hide error msg and success msg
+  $("#errorMsg").hide();
+  $("#successMsg").hide();
 
   //populate year select
   var opt = '';
@@ -9,7 +12,51 @@ $(document).ready(function(){
 
   $("#movieYear").append(opt);
 
-  //submit
-  
+
+  //submit movie ajax
+  $("#submitaddMovie").on("click", function(e) {
+        e.preventDefault();
+
+        //submit movie validation
+        var a = $("#movieTitle").val() != "";
+        var b = $("#movieActors").val() != "";
+        var c = $("#movieGenre").val() != "";
+        var d = $("#movieDescription").val() != "";
+
+        if(a & b & c & d){
+            var jsonObject = {
+                "movieTitle": $("#movieTitle").val(),
+                "movieYear": $("#movieYear option:selected" ).text(),
+                "movieActors": $("#movieActors").val(),
+                "movieGenre": $("#movieGenre").val(),
+                "movieDescription": $("#movieDescription").val(),
+                "action" : "addMovie"
+            };
+
+            console.log(jsonObject);
+
+            $.ajax({
+                type: "POST",
+                url: "data/applicationLayer.php",
+                data: jsonObject,
+                dataType: "json",
+                contentType: "application/x-www-form-urlencoded",
+                success: function(jsonData) {
+                    //alert(jsonData);
+                    $("#errorMsg").hide();
+                    $("#successMsg").show();
+
+                },
+                error: function(errorMsg) {
+                    alert(errorMsg.statusText);
+                }
+            });
+
+    }else{
+      $("#successMsg").hide();
+      $("#errorMsg").show();
+
+    }
+  });
 
 });
