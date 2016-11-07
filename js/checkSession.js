d40a8fea8d1f14a6686077a265823462fdc6599b
andy <andy@localhost.localdomain>
2016-11-06 19:34:45 -0600

    login in all pages

--- js/checkSession.js
+++ js/checkSession.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
       success: function(jsonData) {
           //alert(jsonData);
           if(jsonData['username'] != ""){
-            //alert(jsonData['username']);
+            alert(jsonData['username']);
             $('#addMovieli').show();
           }
           else {
